9fda256d9523159b2212fd5db7d4d2fd7aa90dd2
Arafat Hossain Shesher <116815179+shesherr@users.noreply.github.com>
2025-11-19 02:08:26 +0600

    Delete EHR directory

--- EHR/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EHR.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
